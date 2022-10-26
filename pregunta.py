2d1878459f8caacbf232635b02c4ef8e6bb54cb1
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-26 00:23:22 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,55 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    
+    import pandas as pd
+    import re 
 
+    ### llamar base de datos
+    #def load_file(url):
+    with open("clusters_report.txt", "r") as file:
+      data = file.readlines()
+    
+    data = data[4:]  ## seleccionar sin titulos
+    data = [line.replace("\n","") for line in data]   #organizar espacios y cambios de linea
+    data = [line.strip() for line in data]
+
+    data = data[4:]  ## seleccionar sin titulos
+    data = [line.replace("\n","") for line in data]   #organizar espacios y cambios de linea
+    data = [line.strip() for line in data]
+
+
+    listas = []
+    
+    espacios = ''
+    indice = 0
+    #ciclo para andar renglon por renglon
+    while indice < len(data):   
+        if data[indice] != '':
+            espacios += ' ' + data[indice]
+        else:
+            listas.append(espacios)
+            espacios = ''
+        indice +=1  #contador
+
+    listas = [line.strip() for line in lista]   #quitar espacios
+
+     info = []
+    for index in lista:
+        regular = re.search(r'(^[0-9]+)\W+([0-9]+)\W+([0-9]+)([!#$%&*+-.^_`|~:\[\]]+)(\d+)(\W+)(.+)', index)
+        linea = regular.group(1) + '*' + regular.group(2) + '*' + regular.group(3) + '.' + regular.group(5) + '*' + regular.group(7)
+        info.append(linea)
+    datos = [line.split('*') for line in info]
+
+    df = pd.DataFrame(columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+
+    #convertir lista de listas a df
+    index = 0
+    while index < 4:
+        df[list(df.columns)[index]] = [element[index] for element in datos]  #asignar a cada titulo una columna
+        index +=1
+        
+     df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x : str(x).replace("   "," ").strip())
+     df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x : str(x).replace("  "," ").strip())
+    
     return df
