0c04d5da449b53e0917dc5c1ff07e345cb7b329f
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-26 00:38:32 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -65,10 +65,10 @@ def ingest_data():
         df[list(df.columns)[index]] = [element[index] for element in datos]  #asignar a cada titulo una columna
         index +=1
         
-     df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x : str(x).replace("   "," ").strip())
-     df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x : str(x).replace("  "," ").strip())
-     df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x : str(x).replace("."," ").strip())
+     #df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x : str(x).replace("   "," ").strip())
+     #df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x : str(x).replace("  "," ").strip())
+     #df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x : str(x).replace("."," ").strip())
     
-     df["porcentaje_de_palabras_clave"] = df["porcentaje_de_palabras_clave"].astype('float')
+     #df["porcentaje_de_palabras_clave"] = df["porcentaje_de_palabras_clave"].astype('float')
     
     return df
